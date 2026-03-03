50c299f9c666b52057dd25a5df700c607da89d1d
Devika1612 <devikasiddani@gmail.com>
2026-03-03 17:11:41 +0530

    deploy nginx

---
+++ 0-provider.tf
@@ -0,0 +1,16 @@
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name = "argocd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart = "argo-cd"
+  namespace = "argocd"
+  create_namespace = true
+
+values = [file("argocd.yaml")]
+}
